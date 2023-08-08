4d80856b1dad258b8525b3c341a82559a660086e
Sergio Nunes <sernuness@outlook.com>
2023-08-08 19:18:08 -0300

    v3

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function calcular() {
     var salary = document.querySelector("#salaryin")
-    salary = parseInt(salary.value)
+    salary = Number(salary.value)
     var carg = document.querySelector('#cargahin')
-    carg = parseInt(carg.value)
+    carg = Number(carg.value)
     var porc = document.querySelector('#porcentin')
-    porc = parseInt(porc.value)
+    porc = Number(porc.value)
     var horae = document.querySelector('#horasein')
-    horae = parseInt(horae.value)
+    horae = Number(horae.value)
     if (salary <= 0 || carg <= 0 || porc <= 0 || horae <= 0) {
         window.alert('Está faltando alguma informação, por favor verifique!')
+    } else {
+        var hour = salary / carg
+        var p = porc / 100
+        var h = horae * 60 // preciso entender como converter uma hora em minutos
+        window.alert(`teste ${hour}, ${p}, ${h}`)
     }
 }
